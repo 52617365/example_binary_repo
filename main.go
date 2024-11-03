1c743c9c9e158613646422da1f59f25a66c6bb5e
rase <fake@email.com>
2024-11-03 16:44:05 +0200

    getting pid is now cross platform.

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"runtime"
 	"strconv"
 )
 
@@ -16,12 +17,19 @@ func main() {
 	binary := args[0]
 	mode := args[1]
 	selfPid := os.Getpid()
+	log.Printf("PID: %d\n", selfPid)
 
 	if mode == "run" {
 		log.Println("Simulating work")
 	} else if mode == "update" {
 		// Using exec.Command + Start instead of syscall.Exec because syscall.Exec inherits PID and the other does not since it spawns a subprocess.
-		execCmd := exec.Command("/Users/rase/dev/auto_updating/auto_update_updater/AutoUpdateUpdater", strconv.Itoa(selfPid), binary)
+		var pathToUpdater string
+		if runtime.GOOS == "windows" {
+			pathToUpdater = "C:\\Users\\rase\\Desktop\\AutoUpdateUpdater.exe"
+		} else {
+			pathToUpdater = "/Users/rase/dev/auto_updating/auto_update_updater/AutoUpdateUpdater"
+		}
+		execCmd := exec.Command(pathToUpdater, strconv.Itoa(selfPid), binary)
 		if err := execCmd.Start(); err != nil {
 			log.Fatal(err)
 		}
